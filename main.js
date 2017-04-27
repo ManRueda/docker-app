95d8e8597db4ba212d21f1ac86cf80d541ca0cb2
Manuel Rueda <manuel.rueda.un@gmail.com>
2017-04-26 21:51:29 -0300

    Add react devtools

--- main.js
+++ main.js
@@ -15,6 +15,13 @@ if (process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) |
   dev = true
 }
 
+if (dev) {
+  const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer')
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err))
+}
+
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
