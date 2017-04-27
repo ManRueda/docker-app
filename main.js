2f4cbe66ec340deaa3a2b624dfc49af6b0e0f5f2
Manuel Rueda <manuel.rueda.un@gmail.com>
2017-04-26 22:49:11 -0300

    Add redux devtools

--- main.js
+++ main.js
@@ -22,7 +22,11 @@ function createWindow () {
   })
 
   if (dev) {
-    const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer')
+    const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer')
+
+    installExtension(REDUX_DEVTOOLS)
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((err) => console.log('An error occurred: ', err))
     installExtension(REACT_DEVELOPER_TOOLS)
       .then((name) => console.log(`Added Extension:  ${name}`))
       .catch((err) => console.log('An error occurred: ', err))
