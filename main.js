a2c2a66575269e0d29885daf2821871aa1c24ff2
Manuel Rueda <manuel.rueda.un@gmail.com>
2017-04-27 09:31:53 -0300

    Move extension instalation to the createWindow function to avoid issues

--- main.js
+++ main.js
@@ -15,18 +15,18 @@ if (process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) |
   dev = true
 }
 
-if (dev) {
-  const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer')
-  installExtension(REACT_DEVELOPER_TOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err))
-}
-
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 1024, height: 768, show: false
   })
+
+  if (dev) {
+    const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer')
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((err) => console.log('An error occurred: ', err))
+  }
 
   // and load the index.html of the app.
   let indexPath
